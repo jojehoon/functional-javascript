35c690700793c1f4001d0fc0c8f70b832745c8bd
jhjo <jhjo@reseedcorp.com>
2021-04-09 17:36:58 +0900

    doc: initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var users = [
+  { id: 1, name: 'ID', age: 36 },
+  { id: 2, name: 'BJ', age: 32 },
+  { id: 3, name: 'JM', age: 32 },
+  { id: 4, name: 'PJ', age: 27 },
+  { id: 5, name: 'HA', age: 25 },
+  { id: 6, name: 'JE', age: 26 },
+  { id: 7, name: 'JI', age: 31 },
+  { id: 8, name: 'MP', age: 23 }
+];
+
+// 1. 명령형 코드
+// 1. 30세 이상인 users를 거른다.
+
+const youngerUsers = users.filter(user => user.age < 30)
+console.log(youngerUsers);
