93026314863f40d775499976ecbad8e423f1c8eb
jhjo <jhjo@reseedcorp.com>
2021-04-09 17:58:02 +0900

    doc: 섹션 2. 함수형으로 전환하기 - 회원목록, map, filter 진행중

--- index.js
+++ index.js
@@ -10,7 +10,39 @@ var users = [
 ];
 
 // 1. 명령형 코드
-// 1. 30세 이상인 users를 거른다.
+  // 1. 30세 이상인 users를 거른다.
+  var temp_users = [];
+  for(var i = 0; i < users.length; i++){
+    if(users[i].age >= 30) {
+      temp_users.push(users[i])
+    }
+  }
+  // console.log(temp_users);
+
+  // 2. 30세 이상인 users의 names를 수집한다.
+  var names = [];
+  for(var i = 0; i < temp_users.length; i++){
+    names.push(temp_users[i].name)
+  }
+  // console.log(names);
+
+  // 3. 30세 미만인 users를 거른다.
+  var temp_users = [];
+  for(var i = 0; i < users.length; i++){
+    if(users[i].age < 30) {
+      temp_users.push(users[i])
+    }
+  }
+  // console.log(temp_users);
+  
+  // 4. 30세 미만인 users의 ages를 수집한다.
+  var ages = [];
+  for(var i = 0; i < temp_users.length; i++){
+    ages.push(temp_users[i].age)
+  }
+  console.log(ages);
+
+  // 2. _filter, _map으로 리팩토링
 
 const youngerUsers = users.filter(user => user.age < 30)
-console.log(youngerUsers);
+// console.log(youngerUsers);
